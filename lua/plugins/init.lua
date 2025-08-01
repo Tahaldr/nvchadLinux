280a260968115f995c96e8f55db2595c3cced217
Tahaldr <tahanaitdo@gmail.com>
2025-08-01 03:03:15 +0100

    added laravel.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,6 +18,24 @@ return {
     ft = "blade",
   },
 
+  -- A comprehensive Laravel development plugin for Neovim
+  {
+    "adibhanna/laravel.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>la", ":Artisan<cr>", desc = "Laravel Artisan" },
+      { "<leader>lc", ":Composer<cr>", desc = "Composer" },
+      { "<leader>lr", ":LaravelRoute<cr>", desc = "Laravel Routes" },
+      { "<leader>lm", ":LaravelMake<cr>", desc = "Laravel Make" },
+    },
+    config = function()
+      require("laravel").setup()
+    end,
+  },
+
   -- Highly experimental plugin that completely replaces the UI for messages, cmdline and the popupmenu.
   {
     "folke/noice.nvim",
