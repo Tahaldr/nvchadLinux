462b7c6cfeb53e051d0a12697941837d06a6d85c
Tahaldr <tahanaitdo@gmail.com>
2025-08-18 21:17:13 +0100

    Installed trouble.nvim plug-in

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,46 @@ return {
     end,
   },
 
+  -- A pretty list for showing diagnostics, references, telescope results, quickfix and location lists
+  -- to help you solve all the trouble your code is causing.
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>tx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>tX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>ts",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>tl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>tL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>tQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+
   -- plug-in which provides support for expanding abbreviations similar to emmet.
   -- action key : <C-y> + ,
   {
