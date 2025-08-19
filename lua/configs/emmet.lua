aa0bcc9da475fb1235d7a59ebe2072d0b8bcb46d
Tahaldr <tahanaitdo@gmail.com>
2025-08-19 12:22:09 +0100

    Fixed Separating configs from plugs errors

---
+++ lua/configs/emmet.lua
@@ -0,0 +1,11 @@
+return {
+  ft = {
+    "html",
+    "javascript",
+    "javascriptreact",
+    "less",
+    "sass",
+    "scss",
+    "typescriptreact",
+  }, -- enable for these filetypes
+}
---
+++ lua/configs/laravel.lua
@@ -0,0 +1,11 @@
+return {
+  keys = {
+    { "<leader>la", ":Artisan<cr>", desc = "Laravel Artisan" },
+    { "<leader>lc", ":Composer<cr>", desc = "Composer" },
+    { "<leader>lr", ":LaravelRoute<cr>", desc = "Laravel Routes" },
+    { "<leader>lm", ":LaravelMake<cr>", desc = "Laravel Make" },
+  },
+  config = function()
+    require("laravel").setup()
+  end,
+}
